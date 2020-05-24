31a4e77e6e08d308c8231035ef119e9b7605283b
saifamrani <55050364+saifamrani@users.noreply.github.com>
2020-05-24 18:34:45 +0200

    Update index.js

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,26 +1,46 @@
+const modelClass = document.querySelector('.model');
+const result = document.getElementById('result');
 
-const person = document.querySelector("#person");
-const btn = document.querySelector("#calc");
-const service = document.getElementById("service");
-const result = document.querySelector("#result");
-const each = document.querySelector("#each");
+function calculateTip() {
+  let billAmount = document.getElementById("billamount").value;
+  let serviceRating = document.getElementById("serviceRating").value;
+  let amountOfPeople = document.getElementById("peopleAmount").value;
+  try {
+    if (billAmount == "") throw "Insert in the bill amount please !!!";
+    if (isNaN(billAmount)) throw "Insert any number for bill amount";
+    billAmount = Number(billAmount);
+    if (billAmount <= 0) throw "Insert a number greater then Zero";
 
-function calculte() {
-  const billContentS = document.getElementById("bill").value;
-  let bill = parseInt(billContentS);
-  const serv = parseInt(service.value);
-  const eachPerson = parseInt(person.value);
+    if (serviceRating == 0) throw "Rate the service";
+    serviceRating = Number(serviceRating);
 
-  let percentage = (bill * serv) / 100;
+    if (amountOfPeople == "") throw "Insert no. of people fields";
+    if (isNaN(amountOfPeople)) throw "Please insert any number for the amount of people";
+    amountOfPeople = Number(amountOfPeople);
+    if (amountOfPeople <= 0) throw "Insert a number greater then zero for the number of people!!!";
+    if (Math.floor(amountOfPeople) !== amountOfPeople) throw "Insert an integer number for the amount of people!!!";
+    const total = ((billAmount * serviceRating) / amountOfPeople).toFixed(2);
 
-  let dividedOnpeople = Math.floor(percentage / eachPerson);
+    (amountOfPeople === 1) ? document.getElementById("eachPerson").style.display = "none" : document.getElementById("eachPerson").style.display = "block";
 
-  result.innerText = "$ " + dividedOnpeople;
-  result.style.display = "block";
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
 
-  if (eachPerson > 1) {
-    each.style.display = "block";
+  }
+  catch (errorMessage) {
+    modelClass.style.display = 'block';
+    result.innerHTML = `
+    <h2>Information</h2>
+    <hr>
+    <p>${errorMessage}</p>
+  `;
+    setTimeout(function () { modelClass.style.display = 'none'; }, 2000);
   }
 }
 
-btn.addEventListener("click", calculte); 
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("eachPerson").style.display = "none";
+
+document.getElementById("calculate").addEventListener('click', function () {
+  calculateTip()
+});
